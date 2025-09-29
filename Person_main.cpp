84b7df93a1a0a67674c36f3c6545947213c9c46c
Kevin Silva <149520733+Anauderis@users.noreply.github.com>
2025-09-29 10:17:51 -0400

    Add files via upload

---
+++ Person_main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ksilv on 9/29/2025.
+//
+
+#include "Person.h"
+using namespace std;
+
+int main() {
+    Person p;
+    p.name = "Alice";
+    p.age = 18;
+
+    p.introduce();
+
+    return 0;
+}
